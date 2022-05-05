2b9db4366c2a6ad2fa2d702a1284c422fb900f8a
Youssef Touate <ytouate@e1r7p1.1337.ma>
2022-05-05 06:49:49 +0000

    implementing the echo command without the -n option

--- exec.c
+++ exec.c
@@ -6,11 +6,83 @@
 /*   By: ytouate <ytouate@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 04:58:08 by ytouate           #+#    #+#             */
-/*   Updated: 2022/05/05 05:20:34 by ytouate          ###   ########.fr       */
+/*   Updated: 2022/05/05 06:47:19 by ytouate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main()
+#include <string.h>
+#include "exec.h"
+
+int ft_strlen(char *s)
 {
-    return (0);
+	int count;
+	count = 0;
+	while (*s++)
+		count++;
+	return (count);
+}
+char *ft_join(char **s, int size)
+{
+	int total_lenght;
+	int i;
+	int j;
+	int	k;
+	char *joined_str;
+
+	i = 1;
+	k = 0;
+	total_lenght = 0;
+	while (i < size)
+		total_lenght += ft_strlen(s[i++]);
+	joined_str = malloc(sizeof(char) * (total_lenght + 1) + (i - 2));
+	i = 0;
+	while (++i < size)
+	{
+		j = 0;
+		while (j < ft_strlen(s[i]))
+			joined_str[k++] = s[i][j++];
+		if (i != size -1)
+			joined_str[k++] = ' ';
+	}
+	joined_str[k] = '\0';
+	return (joined_str);
+}
+void ft_echo(int size, char **s, char flag)
+{
+	int	i;
+	i = 0;
+	char *to_be_printed;
+	to_be_printed = ft_join(s, size);
+	if (flag == 'n');
+		// todo
+	else
+	{
+		write(1, to_be_printed, ft_strlen(to_be_printed));
+		write(1, "\n", 1);
+	}
+}
+int ft_strcmp(char *s, char *str)
+{
+	int i;
+	i = 0;
+	while (s[i] || str[i])
+	{
+		if (s[i] != str[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+int main(int ac, char **av)
+{
+	if (ac > 1)
+	{
+		if (ft_strcmp(av[1], "echo") == 0)
+			ft_echo(ac, av, '\0');
+		else if (ft_strcmp(av[1], "cd") == 0)
+			;
+		else
+			write(2, "command not found\n", 19);
+	}
+	
 }
--- exec.h
+++ exec.h
@@ -6,18 +6,17 @@
 /*   By: ytouate <ytouate@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 04:54:13 by ytouate           #+#    #+#             */
-/*   Updated: 2022/05/05 05:15:43 by ytouate          ###   ########.fr       */
+/*   Updated: 2022/05/05 06:48:45 by ytouate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXEC_H
 # define EXEC_H
 
-#include <unistd.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/uio.h>
-#include <unitd.h>
+#include <unistd.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -27,5 +26,7 @@
 #include <sys/stat.h>
 #include <termios.h>
 #include <sys/ioctl.h>
+#include <dirent.h>
+
 
 #endif
